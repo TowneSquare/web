75ac7cc8e7119c6051065b808559f53e82559b76
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2022-07-29 16:45:30 +0100

    commit

---
+++ townesquare/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>TowneSquare Checklist:</h1>
+        <body>Build site: x<br />Win hackathon: x<br />Publish TowneSquare: x<br />Get rich: x</body>
+        <br />
+        <img src="https://media4.giphy.com/media/Qv5TjYPMeesh3ALM4N/giphy.gif"/>
+        <br />
+        <br />
+        <body>Message Dreamer#6463 on Discord when you run out of work to do! :)</body>
+      </center>
+    </div>
+  );
+}
+
+export default App;

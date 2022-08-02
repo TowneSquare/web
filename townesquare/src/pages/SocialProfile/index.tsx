13915a0c83818eeadd8bce291bf25de531c08902
Jenny Jung <jjung9310@gmail.com>
2022-08-01 22:53:54 -0400

    clean up social profile page

--- townesquare/src/pages/SocialProfile/index.tsx
+++ townesquare/src/pages/SocialProfile/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { useTheme } from "@emotion/react";
 import { shortenAddress } from "../../utils/utils";
 import { CoverPhotoWrapper } from "./styles"
 import { Row } from "../../styles/common";
@@ -10,8 +9,6 @@ import { dummyProfile } from "./constants";
 
 const SocialProfile = () => {
   const location = useLocation();
-  const theme = useTheme()
-//   const [profileImg, setProfileImg] = useState<string>();
   const [displayAddress, setDisplayAddress] = useState<string>('');
 
   useEffect(() => {
@@ -26,44 +23,6 @@ const SocialProfile = () => {
           <CoverPhotoWrapper className="cover-photo-container">
             <ProfileCard data={dummyProfile}/>
           </CoverPhotoWrapper>
-          {/* <Row items="center">
-            <FollowInfo className="follow-info">
-              <InfoItem>
-                <p className="row-item-title">Followers</p>
-                <p className="row-item-value">12</p>
-              </InfoItem>
-              <InfoItem>
-              <p className="row-item-title">Following</p>
-                <p className="row-item-value">12</p>
-              </InfoItem>
-            </FollowInfo>
-              <InfoBox className="info-box">
-                  <UsernameInfoBox className="username-info">
-                    <Col align="center">
-                      <p className="username">User123</p>
-                      <button className="address-copy-btn" disabled={!displayAddress} onClick={() => copyTextToClipboard(displayAddress)}>
-                        <p className="abbreviated-wallet-address">{displayAddress}</p>
-                      </button>
-                    </Col>
-                    {showCopied && <CopiedTooltip>
-                      <p>Copied!</p>
-                    </CopiedTooltip>}
-                  </UsernameInfoBox>
-
-                  <Col className="profile-socials" align="start">
-                    <button id="twitter" className="social-link">
-                      Twitter
-                    </button>
-                    <button id="website" className="social-link">
-                      Website
-                    </button>
-                  </Col>
-
-                  <div className="profile-bio">
-                    <p>Hello! This is user's bio. </p>
-                  </div>
-              </InfoBox>
-          </Row> */}
         </Row>
     </Page>
   )

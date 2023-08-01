55059fba4c2e597940402330d1cf2ff11dcffaea
Kirill Zabrodin <me@kirillzabrodin.com>
2023-08-01 18:44:23 +0100

    Fixed hover hand icon

--- src/components/RightPanel/index.tsx
+++ src/components/RightPanel/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import "./rightpanel.css";
+
 interface Stage {
   stage: any;
   isVisible: boolean;
@@ -14,7 +16,7 @@ const RightPanelMenu = (props: Stage) => {
   return (
     <div className={`right-panel-menu fixed right-[-20px] top-1/3 z-50 font-Outfit font-bold text-sm text-[#7F7F7F] transform transition-transform -translate-x-20 ${props.isVisible ? '-translate-x-9' : 'translate-x-full'
       }`}>
-      <ul className='space-y-4 list-disc'>
+      <ul className='space-y-4 list-disc pointer-hover'>
         <li className={props.stage === 'Introduction' ? 'active' : ''} onClick={() => handlePanelClick("homeDiv")}>Introduction</li>
         <li className={props.stage === 'Vision' ? 'active' : ''} onClick={() => handlePanelClick("visionDiv")}>Vision</li>
         <li className={props.stage === 'Features' ? 'active' : ''} onClick={() => handlePanelClick("feature")}>Features</li>
